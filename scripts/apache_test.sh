fb5d031b7e2dbb180bd49b5fa3e84a1d8e86077b
Shishant Biswas <shishantbiswas@gmail.com>
2026-03-31 14:38:56 +0530

    add: middleware setup

--- scripts/apache_test.sh
+++ scripts/apache_test.sh
@@ -1,4 +1,7 @@
-ab -n 1000000 -c 1000 http://localhost:8080/
+ab -n 10000 -c 1000 http://localhost:3000/
+ab -n 10000 -c 1000 http://localhost:3000/favicon.png
+ab -n 10000 -c 1000 http://localhost:3000/user/1337
+ab -n 10000 -c 1000 http://localhost:3000/api/proxy
 
 # siege -c 200 -r 50 http://localhost:8080/
 # wrk -t8 -c200 -d30s http://localhost:8080/
